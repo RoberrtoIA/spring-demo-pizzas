c273ef6f653e13677dc109ba490e85898fbe7a00
RoberrtoIA <robertoantoniomoreno1999@gmail.com>
2026-07-20 18:49:43 -0600

    setup

---
+++ src/main/java/com/pizza/sesamo/SesamoApplication.java
@@ -0,0 +1,13 @@
+package com.pizza.sesamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesamoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pizza/sesamo/SesamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizza.sesamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
