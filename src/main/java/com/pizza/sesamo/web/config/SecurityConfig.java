6fc5f293e1553ecfe19a9754dce34d06b4e086cd
RoberrtoIA <robertoantoniomoreno1999@gmail.com>
2026-07-22 20:21:32 -0600

    Ëjemplo con Cors en CorsConfig

--- src/main/java/com/pizza/sesamo/web/config/SecurityConfig.java
+++ src/main/java/com/pizza/sesamo/web/config/SecurityConfig.java
@@ -12,6 +12,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        http.cors(Customizer.withDefaults());
+
         http.authorizeHttpRequests(auth -> {
             auth.anyRequest().authenticated();
         }).httpBasic(Customizer.withDefaults());
